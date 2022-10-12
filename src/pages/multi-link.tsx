0496b8bfb473d03ca93cf8f2407c4363e0674c50
George Boozin <boozingeorge@gmail.com>
2022-10-12 14:23:47 +0400

    add title

--- src/pages/multi-link.tsx
+++ src/pages/multi-link.tsx
@@ -1,10 +1,19 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import { Header } from "components/HeaderWithLocale";
 import { MultiLink } from "components/MultiLink";
 
 const MultiLinkPage: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Easy Meal | Multilink</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Easy Meal | Food Boxes | Delivery | Tbilisi"
+        />
+      </Head>
       <Header />
       <MultiLink />
     </>
